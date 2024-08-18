02ac9aef1802d628452580f1a9bd6f6ad7641e7a
Larz <larz@Larz.myguest.virtualbox.org>
2024-08-18 20:03:39 +0200

    sefkjhnsdf

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -3,12 +3,45 @@
 
 std::string sed(std::string content, std::string s1, std::string s2)
 {
+	std::string out = "";
+
+	for (std::size_t i = 0; i < content.size(); i++)
+	{
+		std::size_t j = 0;
+
+		while (content[i + j] == s1[j] && s1[j])
+			j++;
+		
+		if (s1[j])
+			out += content[i];
+		else
+		{
+			out += s2;
+			i += j - 1;
+		}
+	}
 	
+	return out;
 }
 
 std::string readFile(std::string filename)
 {
+	std::string out = "";
 
+	std::ifstream file(filename.c_str());
+
+	while (file)
+	{
+		char c;
+		file.read(&c, 1);
+
+		if (file)
+			out += c;
+	}
+
+	file.close();
+
+	return out;
 }
 
 int main(int ac, char **av)
@@ -16,9 +49,9 @@ int main(int ac, char **av)
 	if (ac != 4)
 		return 0;
 	
-	std::string file = av[1];
-	std::string s1 = av[2];
-	std::string s2 = av[3];
+	std::string file(av[1]);
+	std::string s1(av[2]);
+	std::string s2(av[3]);
 
-
+	std::cout << sed(readFile(file), s1, s2) << std::endl;
 }
